82e8442e5c523a9416f9fd46edd3ea89d9667bd1
Kevin Méndez <kevin_mendez30@outlook.com>
2021-09-07 12:23:13 -0600

    frontend: generate a new coffee order

--- public/app.js
+++ public/app.js
@@ -28,10 +28,47 @@ const Main = {
   }
 };
 
+const newCoffe = {
+  template: `
+  <form>
+    <div class="form-group">
+      <label for="name">Nombre</label>
+      <input v-model="coffe.name" type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Digite nombre">
+    </div>
+    <div class="form-group">
+      <label for="coffeInput">Café</label>
+      <input v-model="coffe.type" type="text" class="form-control" id="coffeInput" placeholder="Tipo de café">
+    </div>
+    <button @click.prevent="saveCoffe" class="btn btn-primary">Guardar</button>
+  </form>
+  `,
+  components: {
+    statusValidation
+  },
+  setup() {
+    const coffe = vue.ref({
+      name: 'Kevin Méndez',
+      type: 'Expreso'
+    });
+
+    const saveCoffe = async () => {
+      const coffee = await axios.post('/api/v1/coffee', {
+        ...coffe.value
+      });
+      console.log(coffee);
+    };
+    return { coffe, saveCoffe };
+  }
+};
+
 const routes = [
   {
     path: '/',
     component: Main
+  },
+  {
+    path: '/new-coffe',
+    component: newCoffe
   }
 ];
 
