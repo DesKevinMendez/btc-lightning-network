a96a82bf4eb1353fba967c5f5ea3a153add84e8d
Kevin Méndez <kevin_mendez30@outlook.com>
2021-09-07 11:36:49 -0600

    frontend: verification if node is online

--- public/app.js
+++ public/app.js
@@ -1,25 +1,45 @@
-const Main = {
-  template: '<div @click="click">Hellow from main</div>',
+const vue = Vue;
+const statusValidation = {
+  template: `
+    <div class="alert" :class="[status === 200 ? 'alert-success' : 'alert-danger']" role="alert">
+      Nodo: {{ status === 200 ? 'activo' : 'inactivo' }}
+    </div>
+`,
   setup() {
-    const click = ()=>{
-      console.log('Click');
-    }
-    return {click}
+    let status = vue.ref(404);
+
+    vue.onMounted(async () => {
+      const info = await axios.get('/api/v1/lnd/info');
+      status.value = info.status;
+    });
+    return { status };
   }
-}
+};
+
+const Main = {
+  template: `
+    <status-validation />
+  `,
+  components: {
+    statusValidation
+  },
+  setup() {
+    return {};
+  }
+};
 
 const routes = [
   {
-    path: "/",
-    component: Main,
-  },
+    path: '/',
+    component: Main
+  }
 ];
 
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes
-})
+});
 
-const app = Vue.createApp({})
-app.use(router)
-app.mount('#app')
+const app = vue.createApp({});
+app.use(router);
+app.mount('#app');
